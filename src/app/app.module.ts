57282b95c20e6527115595910d136765a7a2463c
Xavier Yanza <xavier.y.2013@gmail.com>
2024-07-03 19:03:52 -0500

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { OrdersComponent } from './components/orders/orders.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzButtonModule,
+    FormsModule,
+    NzSelectModule,
+    NzRadioModule,
+    NzCheckboxModule,
+    NzFormModule,
+    NzInputModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/orders/orders.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent {
+
+  form:UntypedFormGroup;
+
+  types:any[] = [
+    {
+      code:1,
+      name:'Analgésico'
+    },
+    {
+      code:2,
+      name:'Analéptico'
+    },
+    {
+      code:3,
+      name:'Anestésico'
+    },
+    {
+      code:4,
+      name:'Antiácido'
+    },
+    {
+      code:5,
+      name:'Antidepresivo'
+    },
+    {
+      code:6,
+      name:'Antibioticos'
+    }
+  ]
+
+  constructor(private fb:FormBuilder){
+    this.buildForm();
+  }
+
+
+  buildForm(){
+    this.form  = this.fb.group({
+      medicine:[null,[Validators.required,Validators.minLength(3),Validators.maxLength(250)]],
+      type:[null,[Validators.required]],
+      quantity:[null,[Validators.required,Validators.min(3),Validators.max(100)]],
+      distributor:[null,[Validators.required]],
+    });
+  }
+}
